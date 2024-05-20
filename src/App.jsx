e1b57d993405b63b0e5c0908657e38b3c3dd89d5
priyans619 <priyansh.2b@gmail.com>
2024-05-20 19:44:43 +0530

    Wrapping theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "./components/Theme";
 import Login from "./components/Login";
 import Chatbot from "./components/Chatbot";
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 };
 
