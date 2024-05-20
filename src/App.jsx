2a289fcf59daefef3fe25ef72eaa5c963ec5b078
priyans619 <priyansh.2b@gmail.com>
2024-05-20 19:18:55 +0530

    Add route to Chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Chatbot from "./components/Chatbot";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </div>
   );
