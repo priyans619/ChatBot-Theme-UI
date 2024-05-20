cda96307757f6f61ee6d1bff4176e23ade3f1f28
priyans619 <priyansh.2b@gmail.com>
2024-05-21 00:07:47 +0530

    Add Custom theme

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,27 +1,36 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
+
+const ThemeContext = createContext();
 
 const themes = [
+  { background: "linear-gradient(239.26deg, #DDEEED 63.17%, #FDF1E0 94.92%)", bubble: "bg-white" },
   { background: "bg-blue-300", bubble: "bg-indigo-100" },
   { background: "bg-green-300", bubble: "bg-green-100" },
   { background: "bg-yellow-300", bubble: "bg-yellow-100" },
   { background: "bg-red-300", bubble: "bg-red-100" },
+  { background: "bg-orange-300", bubble: "bg-orange-100" },
+  
 ];
 
-const ThemeContext = createContext();
-
 export const ThemeProvider = ({ children }) => {
-  const [themeIndex, setThemeIndex] = useState(0);
+  const [themeIndex, setThemeIndex] = useState(() => {
+    const savedThemeIndex = sessionStorage.getItem('themeIndex');
+    return savedThemeIndex !== null ? parseInt(savedThemeIndex, 10) : 0;
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem('themeIndex', themeIndex);
+  }, [themeIndex]);
 
   const cycleTheme = () => {
-    setThemeIndex((prevIndex) => (prevIndex + 1) % themes.length);
+    setThemeIndex((prevThemeIndex) => (prevThemeIndex + 1) % themes.length);
   };
 
-  const value = {
-    theme: themes[themeIndex],
-    cycleTheme,
-  };
-
-  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={{ theme: themes[themeIndex], cycleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => useContext(ThemeContext);
